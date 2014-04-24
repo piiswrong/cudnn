a7147890b0d2b12d58e93b48a6c40ebb5ece3b39
John Anderson <sontek@gmail.com>
2014-04-24 10:41:12 -0700

    nothing

--- src/rank.py
+++ src/rank.py
@@ -12,9 +12,12 @@ mat1 = np.dot(np.random.normal(0.0, 1.0, (V1, R1)), np.random.normal(0.0, 1.0, (
 mat2 = np.random.normal(0.0, 1.0, (V2, dim))
 mat = np.concatenate((mat1, mat2), axis = 0)
 
+def rank(M):
+    u,s,v = np.linalg.svd(M)
+    return np.sum( s > 1e-10 )
 
 for i in xrange(100):
     ind = [ i for i in xrange(V) if random.random() < p ]
-    print len(ind), np.linalg.matrix_rank(mat[ind, :])
+    print len(ind), np.linalg.matrix_rank(mat[ind, :]), rank(mat[ind, :])
 
 
